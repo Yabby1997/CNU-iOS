9def9c9abf69abf1bed0eb7e1fcf898d303f6c3b
yabby <yabby1997@gmail.com>
2021-09-04 21:14:34 +0900

    1주차 토요일 자료 완성

--- week1/Completed/basics/basics/ViewController.swift
+++ week1/Completed/basics/basics/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var pinkView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        dump(self.view)
+        topView.backgroundColor = .red
+        topButton.backgroundColor = .systemIndigo
+        topButton.setTitleColor(.white, for: .normal)
+        topButton.setTitle("버튼", for: .normal)
+        
+        print(self.view.frame)
+        print(self.view.bounds)
+        print(topView.frame)
+        print(topView.bounds)
+        print(greenView.frame)
+        print(greenView.bounds)
+        print(pinkView.frame)
+        print(pinkView.bounds)
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        topView.backgroundColor = .systemIndigo
+    }
+    
 }
 
