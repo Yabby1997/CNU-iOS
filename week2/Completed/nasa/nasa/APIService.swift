f9ec6db0c3d092acaf64c18f3c134e809ed4ee38
yabby <yabby1997@gmail.com>
2021-09-12 20:46:21 +0900

    2주차 일요일 완성

--- week2/Completed/nasa/nasa/APIService.swift
+++ week2/Completed/nasa/nasa/APIService.swift
@@ -8,14 +8,22 @@
 import Foundation
 
 class APIService {
-    static let apiURL = "https://api.nasa.gov/planetary/apod?api_key=QfWtN1P4Zn0uvmFUZN3PsuhuXoFg0XuroJL2F0Ci&count=50"
+    static let apiKey = "발급받은 NASA API 키를 여기에 넣어주세요"
+    static let fetchCount = 10
+    static let apiURL = "https://api.nasa.gov/planetary/apod?api_key=\(apiKey)&count=\(fetchCount)"
 
-    static func fetch(completion: @escaping ([APODResponse]) -> ()) {
+    static func fetchAPOD(completion: @escaping ([APODResponse]) -> ()) {
         URLSession.shared.dataTask(with: URL(string: apiURL)!) { data, response, error in
             if let _ = error { return }
             guard let data = data else { return  }
             guard let responses = try? JSONDecoder().decode([APODResponse].self, from: data) else { return }
             completion(responses.filter{ $0.type == "image" })
         }.resume()
+    }
+    
+    static func fetchData(url: String, completion: @escaping (Data) -> ()) {
+        guard let url = URL(string: url),
+              let data = try? Data(contentsOf: url) else { return }
+        completion(data)
     }
 }
